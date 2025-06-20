290ce911b9bd47a19b7bd344344af4ef32f4b0be
Ronin <bhabyaprakash01@gmail.com>
2025-06-20 20:52:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,26 +4,16 @@ from os import getenv
 from dotenv import load_dotenv
 from pyrogram import filters
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Telegram API credentials (from https://my.telegram.org)
+# --------------------------------- #
+
 API_ID = 20948356
 API_HASH = "6b202043d2b3c4db3f4ebefb06f2df12"
-
-# Bot token from @BotFather
 BOT_TOKEN = "7703532604:AAEZO0Nk98N7EJcncvPzeoX1EqGi4L0_b5k"
-
-# MongoDB connection string
 MONGO_DB_URI = "mongodb+srv://Combobot:Combobot@combobot.4jbtg.mongodb.net/?retryWrites=true&w=majority&appName=Combobot"
 
-String_client_1 = "BQGPvYcAhUkfHf38wIvJ_KxeXhQ7McM6oBCteXPzd5DsP3qKHq4gV7WKwg-5r7j1X1Kgtzr6kVKBLRO8JW4VLXIlnKM-31qCIuC05o-rNuDnz3rXWHwPMRGMMrUlEisOAhSg6kp5-9Qa9bcAoIE3OQj3WpOTTNR57diTMojazxUc7MN2zBs8MXrQ5os9FzvKfh9Sg6TvRvvHBjLLMQn6CR8dtXXPyJI3mrTMy7GOIlUKk1eYHep_U_2jnpHFLmNEWOSdbh7F33q4wcnVVbAbf4C859f_lLOF4RgYVHdQqYoglM2tBzJs8aArcHaw5KVRu_0BqwTOSJi-y2WzVCgcXSYVA36yMgAAAAHR1tKRAA"
-String_client_2 = ""
-String_client_3 = ""
-Mustjoin = "Phoenix_HLP"
-
-
-# --------------start.py-------------
+# --------------------------------- #
 
 START_VIDEO = "https://i.ibb.co/nsyp67FS/Img2url-bot.jpg"
 HELP_MENU_VIDEO = "https://i.ibb.co/Z64Z3yCR/Img2url-bot.jpg"
@@ -32,25 +22,26 @@ HELP_VIDEO_2 = "https://i.ibb.co/S7Z4fHJt/Img2url-bot.jpg"
 HELP_VIDEO_3 = "https://unitedcamps.in/Images/file_11453.jpg"
 HELP_VIDEO_4 = "https://unitedcamps.in/Images/file_11454.jpg"
 
-#------------------------------------
+# --------------------------------- #
 
 LOGGER_ID = -1002059639505
 STATS_VIDEO = "https://unitedcamps.in/Images/file_5250.jpg"
 OWNER_ID = 7394132959
 
-# Heroku deployment
+# --------------------------------- #
+
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-# Support
+# --------------------------------- #
+
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PacificArc")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/phoenixXsupport")
 
-# Validate URLs
-if SUPPORT_CHANNEL:
-    if not re.match(r"(?:http|https)://", SUPPORT_CHANNEL):
-        raise SystemExit("[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://")
+# --------------------------------- #
 
-if SUPPORT_CHAT:
-    if not re.match(r"(?:http|https)://", SUPPORT_CHAT):
-        raise SystemExit("[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://")
+if SUPPORT_CHANNEL and not re.match(r"(?:http|https)://", SUPPORT_CHANNEL):
+    raise SystemExit("[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://")
+
+if SUPPORT_CHAT and not re.match(r"(?:http|https)://", SUPPORT_CHAT):
+    raise SystemExit("[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://")
